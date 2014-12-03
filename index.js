7b8b41cd685932862b28a58889b4ab1fb3426cef
nighca <nighca@live.cn>
2014-12-03 13:34:08 +0800

    debug logger

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var logger = require('log4js').getLogger();
 var task = require('./lib/task');
 
 task.on('fetch-channel', function(channel){
-    logger.log('channel fetched', channel.title);
+    logger.info('channel fetched', channel.title);
 });
 
 task.on('save-channel', function(channel){
-    logger.log('channel saved', channel.title);
+    logger.info('channel saved', channel.title);
 });
 
 task.on('save-item', function(item){
-    logger.log('item saved', item.title);
+    logger.info('item saved', item.title);
 });
 
 task.on('error', function(err){
